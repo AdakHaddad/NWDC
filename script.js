85e8335c2b815209407c25c3547058021a3aa493
anggito-ma <anggitobbk@gmail.com>
2023-05-16 14:05:59 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const menubar = document.querySelector(".menu-bar");
+const menunav = document.querySelector(".menu-navigation");
+
+menubar.addEventListener("click", function () {
+  menunav.classList.toggle("menu-click" );
+});
+
+
