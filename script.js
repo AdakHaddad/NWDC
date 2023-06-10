f7449adcf1d79bf4ecd31e8ba79398b1ead5f347
AdakHaddad <muhammadmuqtada@gmail.com>
2023-06-10 21:43:22 +0700

    Todo App starter + mongodb (sedikit bug di button edit)

--- script.js
+++
@@ -1,8 +0,0 @@
-const menubar = document.querySelector(".menu-bar");
-const menunav = document.querySelector(".menu-navigation");
-
-menubar.addEventListener("click", function () {
-  menunav.classList.toggle("menu-click" );
-});
-
-
